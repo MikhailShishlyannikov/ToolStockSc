8e5718c337a44a201b5434bcf251415b958651c7
MikhailShishlaynnikov <mikhailshishlyannikov@gmail.com>
2019-07-09 16:49:16 +0300

    Web project and Jumbotron component

---
+++ Feature/Sam.Feature.Jumbotron/Sam.Feature.Jumbotron/Areas/Feature/Jumbotron/JumbotronAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sam.Feature.Jumbotron.Areas.Jumbotron
+{
+    public class JumbotronAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Jumbotron";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Jumbotron_default",
+                "Jumbotron/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Project/Sam.ToolStockSc.Web/Areas/Project/ToolStockSc/ToolStockScAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Sam.ToolStockSc.Web.Areas.ToolStockSc
+{
+    public class ToolStockScAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ToolStockSc";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ToolStockSc_default",
+                "ToolStockSc/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
