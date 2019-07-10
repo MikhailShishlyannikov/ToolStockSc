0017629e340f9e1530e704b33ee7b5db78354458
MikhailShishlaynnikov <mikhailshishlyannikov@gmail.com>
2019-07-10 11:26:17 +0300

    Update .gitignore

---
+++ Feature/Sam.Feature.Button/Areas/Feature/Button/Controllers/ButtonController.cs
@@ -0,0 +1,24 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using Sam.Foundation.GlassMapper.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sam.Feature.Button.Areas.Feature.Button.Controllers
+{
+    public class ButtonController : BaseController
+    {
+        public ButtonController(IMvcContext mvcContext) : base(mvcContext)
+        {
+
+        }
+
+        // GET: Feature/Button
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
