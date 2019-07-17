17a8d27daeafa8b22c0c763cdac1bf0d3e66058c
MikhailShishlaynnikov <mikhailshishlyannikov@gmail.com>
2019-07-17 10:47:25 +0300

    Some js scripts for right work

---
+++ Project/Sam.ToolStockSc.Web/Scripts/CustomScript.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+const PHONE_INPUT_MASK = '+375 (99) 999-99-99';
+
+$('document').ready(function () {
+    //$.ajaxSetup({ cache: false });
+
+    //$('#datetimepicker').datetimepicker(
+    //    {
+    //        format: 'DD/MM/YYYY',
+    //        locale: 'en-GB',
+    //        minDate: moment(),
+    //        //defaultDate : moment(),
+    //        maxDate: moment().add(7, 'days')
+    //    });
+    //$('#timepicker').datetimepicker(
+    //    {
+    //        format: 'HH:mm',
+    //        locale: 'en-GB',
+    //        //defaultDate: moment(),
+    //        minDate: moment({ h: 9 }),
+    //        maxDate: moment({ h: 22 })
+    //        //datepicker: false
+    //    });
+
+    /////////////Phone Mask
+    $("#phone").inputmask(PHONE_INPUT_MASK);
+
+    $('.spoiler-explore').click(function () {
+        $(this).parent().children('div.spoiler-content').toggle('fast');
+        return false;
+    });
+});
